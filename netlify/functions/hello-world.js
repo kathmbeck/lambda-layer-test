8c3885a4aa427fbf66e1909e1130fe800880fba4
Jenae Janzen <jenae.janzen@netlify.com>
2023-06-02 11:44:04 -0400

    add function

---
+++ netlify/functions/hello-world.js
@@ -0,0 +1,9 @@
+const handler = async (event, context) => {
+  console.log("Hello from the function")
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" }),
+  };
+};
+
+export { handler };
