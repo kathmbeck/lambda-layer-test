971b18f83e46a89deae89484a8bb7750be73a40d
Jenae Janzen <jenae.janzen@netlify.com>
2023-08-18 10:53:12 -0400

    new message

--- netlify/functions/anotha-background.js
+++ netlify/functions/anotha-background.js
@@ -1,5 +1,5 @@
 const handler = async (event, context) => {
-  console.log("Hello from the function lalalala")
+  console.log("Hello from the function!!!!")
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello World 👋 ❤️" }),
