3dd764474a14fc1558987abf711074594835bd66
Jenae Janzen <jenae.janzen@netlify.com>
2023-08-28 14:56:07 -0400

    set timeout

--- netlify/functions/anotha-background.js
+++ netlify/functions/anotha-background.js
@@ -1,9 +1,12 @@
 const handler = async (event, context) => {
-  console.log("Hello from the function!!!!")
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World 👋 ❤️" }),
-  };
+  console.log("Hello from the function Starting a timeout!!!!")
+  setTimeout(() => {
+    console.log("Goodbye from the function!!!")
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World 👋 ❤️" }),
+    }
+  }, 500_001)
 };
 
 export { handler };
