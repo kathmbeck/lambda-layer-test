93594afae5816191ce75746e1de2a0bd6c0a6dd7
Jenae Janzen <jenae.janzen@netlify.com>
2023-08-29 09:57:40 -0400

    add delay

--- netlify/functions/anotha-background.js
+++ netlify/functions/anotha-background.js
@@ -1,12 +1,16 @@
 const handler = async (event, context) => {
-  console.log("Hello from the function Starting a timeout!!!!")
-  setTimeout(() => {
-    console.log("Goodbye from the function!!!")
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello World 👋 ❤️" }),
-    }
-  }, 500_001)
-};
+  console.log("Hello from the function. Starting a timeout!!!!")
+
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+  await delay(600_000)
+
+  console.log("Goodbye from the function!!!")
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World 👋 ❤️" })
+  }
+}
 
 export { handler };
