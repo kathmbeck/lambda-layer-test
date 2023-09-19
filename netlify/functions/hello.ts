f53b633f4a33a76d22d02d118e8f3db50a3cfa1b
Jenae Janzen <jenae.janzen@netlify.com>
2023-09-19 09:13:23 -0400

    try to set a cookie

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,8 @@
 import { Context } from "@netlify/functions"
+import { Cookie } from '@netlify/node-cookies';
 
 export default async (req: Request, context: Context) => {
-  console.log(context.account, context.cookies, context.geo, context.deploy, context.ip, context.json, context.log, context.next, context.params)
+  var cookie:Cookie = { name: "cookie!", value: "yum!"}
+  console.log(context.account, context.cookies.set(cookie), context.geo, context.deploy, context.ip, context.json("lalalala: true"), context.log("huzza!"), context.next, context.params)
   return new Response("Hello, world!")
 }
