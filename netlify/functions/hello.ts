69b949e121e2ff7bbf934b04295748a70fe0c4de
Jenae Janzen <jenae.janzen@netlify.com>
2023-09-19 08:38:56 -0400

    test context properties

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,6 +1,6 @@
 import { Context } from "@netlify/functions"
 
 export default async (req: Request, context: Context) => {
-  console.log(context.account)
+  console.log(context.account, context.cookies, context.geo, context.deploy, context.ip, context.json, context.log, context.next, context.params)
   return new Response("Hello, world!")
 }
