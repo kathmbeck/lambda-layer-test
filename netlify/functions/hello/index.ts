10538b3f31426fcb4653c0fd813db73e9ba80375
Jenae Janzen <jenae.janzen@netlify.com>
2023-09-21 07:20:32 -0400

    inline url config

--- netlify/functions/hello/index.ts
+++ netlify/functions/hello/index.ts
@@ -1,5 +1,11 @@
-import type { Context } from "@netlify/functions"
+import { Config, Context } from "@netlify/functions"
 
 export default async (req: Request, context: Context) => {
-  return new Response("Hello, world!")
+	const { city, country } = context.params
+
+  return new Response(`You're visiting ${city} in ${country}!`)
+}
+
+export const config: Config = {
+  path: "/travel-guide/:city/:country"
 }
