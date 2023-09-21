1b969ce78931725f495cf8858094ff52eee98bb3
Jenae Janzen <jenae.janzen@netlify.com>
2023-09-21 09:12:18 -0400

    remove custom path for other test

--- netlify/functions/lalala.ts
+++ netlify/functions/lalala.ts
@@ -2,7 +2,7 @@ import { Context } from "@netlify/functions"
 import { Cookie } from '@netlify/node-cookies';
 
 export default async (req: Request, context: Context) => {
-  const s = Netlify.env.get(Secret)
+  const s = Netlify.env.get("Secret")
   console.log(s)
   return new Response("Hello, world!")
 }
