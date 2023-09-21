d059ce76542bd461f08cda07946dd7d169d5b1fd
Jenae Janzen <jenae.janzen@netlify.com>
2023-09-21 09:07:18 -0400

    try an env var

--- netlify/functions/lalala.ts
+++ netlify/functions/lalala.ts
@@ -2,7 +2,7 @@ import { Context } from "@netlify/functions"
 import { Cookie } from '@netlify/node-cookies';
 
 export default async (req: Request, context: Context) => {
-  var cookie:Cookie = { name: "cookie!", value: "yum!"}
-  console.log(context.account, context.cookies.set(cookie), context.geo, context.deploy, context.ip, context.json("lalalala: true"), context.log("huzza!"), context.next, context.params)
+  const s = Netlify.env.get(Secret)
+  console.log(s)
   return new Response("Hello, world!")
 }
