14092e40960a8425398285d1dac18eacae6292dc
Jenae Janzen <jenae.janzen@netlify.com>
2023-10-11 15:28:29 -0400

    test

--- netlify/edge-functions/lalala.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from "@netlify/functions"
-import { Cookie } from '@netlify/node-cookies';
-
-export default async (req: Request, context: Context) => {
-  const s = Netlify.env.get("Secret")
-  console.log(s)
-  return new Response("Hello, world!")
-}
