2da95997be21da7db1e0b14c7470a75ba9248395
Jenae Janzen <jenae.janzen@netlify.com>
2024-01-16 10:01:18 -0500

    add private npm package

--- netlify/functions/cookie.ts
+++ netlify/functions/cookie.ts
@@ -1,9 +1,12 @@
 import { Context } from "@netlify/functions"
+import { components } from '@netlify/bitballoon-openapi';
 
 export default async (req: Request, context: Context) => {
   if (context.cookies.get("chocolate")) {
     return new Response("Sorry, no more cookies for you")
   }
+
+  console.log(components)
 
   context.cookies.set("chocolate", "yummy")
 
