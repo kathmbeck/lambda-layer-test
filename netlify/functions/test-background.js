98241a38983468fa74d82014f869ee47b7447962
Jenae Janzen <jenae.janzen@netlify.com>
2024-03-08 11:40:05 -0500

    env var

--- netlify/functions/test-background.js
+++ netlify/functions/test-background.js
@@ -1,3 +1,5 @@
 export default async (req, context) => {
+    const name = process.env.AWS_LAMBDA_FUNCTION_NAME
+    console.log(`Hello from ${name}`);
     return new Response("Hello, world!");
 };
