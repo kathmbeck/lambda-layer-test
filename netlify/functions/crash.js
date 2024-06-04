c62cb27fe4cc6b087fbd2fe8534c62e078dbf770
Jenae Janzen <jenae.janzen@netlify.com>
2024-06-04 10:19:18 -0400

    new error

--- netlify/functions/crash.js
+++ netlify/functions/crash.js
@@ -2,5 +2,8 @@ export default async (req, context) => {
     console.log("hi hi hi")
     console.log("another log")
     console.log("um(last_1m):( ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() ) / ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() + sum:ats.global")
-    throw new Error("Crash!!!! Oh noooooo! um(last_1m):( ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() ) /n ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() + /n sum:ats.global");
+    throw new Error({
+        "Hello":"world",
+        "test":"something"
+     });
   };
