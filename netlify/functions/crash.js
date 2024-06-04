93f748d7d3924bf733e270b86fb5f0700b0194db
Jenae Janzen <jenae.janzen@netlify.com>
2024-06-04 09:22:21 -0400

    add function

---
+++ netlify/functions/crash.js
@@ -0,0 +1,6 @@
+export default async (req, context) => {
+    console.log("hi hi hi")
+    console.log("another log")
+    console.log("um(last_1m):( ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() ) / ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() + sum:ats.global")
+    throw new Error("Crash!!!! Oh noooooo!");
+  };
