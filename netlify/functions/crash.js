627bbb2e005c7f643ac72ba42b40bb1d03ec279b
Jenae Janzen <jenae.janzen@netlify.com>
2024-06-04 10:19:47 -0400

    add log

--- netlify/functions/crash.js
+++ netlify/functions/crash.js
@@ -2,6 +2,10 @@ export default async (req, context) => {
     console.log("hi hi hi")
     console.log("another log")
     console.log("um(last_1m):( ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() ) / ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() + sum:ats.global")
+    console.log({
+        "Hello":"world",
+        "test":"something"
+     })
     throw new Error({
         "Hello":"world",
         "test":"something"
