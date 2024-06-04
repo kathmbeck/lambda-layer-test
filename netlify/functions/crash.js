d34c717d58c30eb081d8491b276d43ebb3f99a82
Jenae Janzen <jenae.janzen@netlify.com>
2024-06-04 14:23:16 -0400

    more json

--- netlify/functions/crash.js
+++ netlify/functions/crash.js
@@ -4,10 +4,26 @@ export default async (req, context) => {
     console.log("um(last_1m):( ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() ) / ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() + sum:ats.global")
     console.log({
         "Hello":"world",
-        "test":"something"
+        "test":"something",
+        "long":"json",
+        "something":"else",
+        "more":"stuff",
+        "blah":"blah",
+        "another":"thing",
+        "and":"another",
+        "return":"statement",
+        "okay":"bye"
      })
     throw new Error({
         "Hello":"world",
-        "test":"something"
+        "test":"something",
+        "long":"json",
+        "something":"else",
+        "more":"stuff",
+        "blah":"blah",
+        "another":"thing",
+        "and":"another",
+        "return":"statement",
+        "okay":"bye"
      });
   };
