f216c6181ec79ed49cd94f49316e8c07b57af520
Jenae Janzen <jenae.janzen@netlify.com>
2024-06-27 15:31:57 -0400

    more logs

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -13,12 +13,12 @@ export default async (req, context) => {
    console.log(req)
    console.log({
     "2222":"2222",
-   "someJson":"loggy-log",
-   "another":"log",
-   "frogOnA":"log",
-   "boblaw":"lawblog",
-   "blahblah":"log",
-   "something":"lalalalala"
+   "somaefoiahefaegfeJson":"log h3wifhefgy-log",
+   "anotaflzdulaelhher":"log",
+   "frogOvrarvaenA":"log",
+   "boblaefafeaefaw":"lawblog",
+   "blaaefarhkydhblah":"log",
+   "soagrarhathdmykukmething":"lalalalala"
   })
   console.log("In the current implementation Ingesteer looks if a given batch contains the platform.start or the platform.runtimeDone events, which works fine as long as there’s a maximum of 2 batches (first batch will contain the platform.start and the last batch will contain platform.runtimeDone.This can cause that due to limitation #2 when having more than 2 batches the intermediary batches will have no AWS Request ID as we will not be able to find platform.start nor platform.runtimeDone on those batches.In the current implementation Ingesteer looks if a given batch contains the platform.start or the platform.runtimeDone events, which works fine as long as there’s a maximum of 2 batches (first batch will contain the platform.start and the last batch will contain platform.runtimeDone.This can cause that due to limitation #2 when having more than 2 batches the intermediary batches will have no AWS Request ID as we will not be able to find platform.start nor platform.runtimeDone on those batches.")
   console.log(23918723918273912873912873918273981273982173981789127389273891732981371471625)
@@ -26,5 +26,9 @@ export default async (req, context) => {
   console.log("ayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
   console.log("remove sleep")
   console.log("✨")
+  console.log("efiahleuifhalweiufaliehflawuegfliawegfiluaegfiluagwefilugaelfiugaweiulfgaweiufgaiuwegfliuaegfiuaegw;fihaew;fhiiwugEFKUAYGEFLIUweglIUWGDFLAIFUWEGFILAUEGWFUgeilfugaeilufgaiulegflaiuegfiluageflugefaluafehufeahlueflgaefguilfaelguefagluaeflgiaefifaeluiefalgfeagliuefalgiefagluefaglufeagluefagluaefgluiaefgluaefgluiaeflguaeflgaefgllaeflgaefgluaefglaefgluiaeflguiafegluiefalguaefgulieaflguiaeflgaefgluaeflgaeflguefagluaeflgaeflgiaeflgfaeluaeflguaefgluaef")
+  console.log("felahufhhfaeluhaefhluaeflhuaefluhefauhlaeflhluhafehulaeflhfaeflahefhlaufahlueafehluflaehuafhluafehluafehluahlfuhluafehluafelhuafehlufaelhuafehluafehlafehluafhluafealefhuaeflhulhufaeaeflhualhfualfhuafhluaeflhulaefhaflhlaehuflahuefaeflhu")
+  console.log("efiahleuifhalweiufaliehflawuegfliawegfiluaegfiluagwefilugaelfiugaweiulfgaweiufgaiuwegfliuaegfiuaegw;fihaew;fhiiwugEFKUAYGEFLIUweglIUWGDFLAIFUWEGFILAUEGWFUgeilfugaeilufgaiulegflaiuegfiluageflugefaluafehufeahlueflgaefguilfaelguefagluaeflgiaefifaeluiefalgfeagliuefalgiefagluefaglufeagluefagluaefgluiaefgluaefgluiaeflguaeflgaefgllaeflgaefgluaefglaefgluiaeflguiafegluiefalguaefgulieaflguiaeflgaefgluaeflgaeflguefagluaeflgaeflgiaeflgfaeluaeflguaefgluaef")
+  console.log("INTERNAL SCREAMING")
     return new Response("Helloooooooooo, world!");
   };
