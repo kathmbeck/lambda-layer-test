42f3f7f26ceffa723008b673c4c39412362bda0c
Jenae Janzen <jenae.janzen@netlify.com>
2024-08-26 10:35:34 -0400

    add func

--- netlify/functions/crash.js
+++ netlify/functions/crash.js
@@ -1,30 +1,10 @@
 export default async (req, context) => {
-    console.log("hi hi hi")
-    console.log("another log")
-    console.log("um(last_1m):( ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() ) / ( sum:ats.global.proxy.process.http.3xx_responses{group:global}.as_count() + sum:ats.global.proxy.process.http.2xx_responses{group:global}.as_count() + sum:ats.global")
-    console.log("log")
-    console.log({
-        "Hello":"world",
-        "this":"is",
-        "long":"json",
-        "from":"the",
-        "console":"log",
-        "blah":"blah",
-        "another":"thing",
-        "not":"the",
-        "return":"statement",
-        "okay":"bye"
-     })
-    throw new Error({
-        "Hello":"world",
-        "test":"something",
-        "long":"json",
-        "something":"else",
-        "more":"stuff",
-        "blah":"blah",
-        "another":"thing",
-        "and":"another",
-        "return":"statement",
-        "okay":"bye"
-     });
+    let seconds = 0;
+    const intervalId = setInterval(() => {
+        console.log(seconds);
+        seconds++;
+        if (seconds === 30) {
+            clearInterval(intervalId);
+        }
+    }, 1000);
   };
