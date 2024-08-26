5f72138fc25ae8378e62214df22cf8444db69cbd
Jenae Janzen <jenae.janzen@netlify.com>
2024-08-26 10:54:58 -0400

    fix func

--- netlify/functions/crash.js
+++ netlify/functions/crash.js
@@ -1,10 +1,15 @@
 export default async (req, context) => {
-    let seconds = 0;
-    const intervalId = setInterval(() => {
-        console.log("count", seconds);
-        seconds++;
-        if (seconds === 30) {
-            clearInterval(intervalId);
+    for (let i = 0; i < 31; i++) {
+        if (i < 30) {
+            console.log("count", i);
+            await sleep(1000);
+        } else {
+            console.log("complete!")
+
         }
-    }, 1000);
-  };
+    }
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
