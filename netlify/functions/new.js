1a51630cb12ce068e1fc8668be4cbf3919f4ed16
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-02-03 11:03:48 -0500

    large log

--- netlify/functions/new.js
+++ netlify/functions/new.js
@@ -1,9 +1,12 @@
-export default async function(req) {
+export default async function (req) {
   const headers = new Headers({
-    'netlify-cdn-cache-control': 'durable, max-age=3600',
-    'netlify-vary': query="",
+    "netlify-cdn-cache-control": "durable, max-age=3600",
+    "netlify-vary": query = "",
   });
 
-  const url = new URL(req.url);
-  return new Response('hello, new here', { headers });
+  // Generate a response that exceeds 150,000 bytes
+  const largeResponse = "A".repeat(160000); // 160 KB of "A"s
+
+  return new Response(largeResponse, { headers });
 }
+
