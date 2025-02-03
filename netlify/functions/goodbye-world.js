4683d2e40a92a00353eab6886460840ff53781a6
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-02-03 12:58:20 -0500

    Update goodbye-world.js

--- netlify/functions/goodbye-world.js
+++ netlify/functions/goodbye-world.js
@@ -5,7 +5,7 @@ export default async function(req) {
 
   const url = new URL(req.url);
   console.log('This is my new function!') 
-  const largeLog = "A".repeat(160000)
+  const largeLog = "A".repeat(16)
   console.log(largeLog)
   return new Response('goodbye world!!!!!!', { headers });
 }
