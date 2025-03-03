b59a687da60e14ddb173660010a2495546a2beb2
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-03-03 16:13:43 -0500

    Update goodbye-world.js

--- netlify/functions/goodbye-world.js
+++ netlify/functions/goodbye-world.js
@@ -7,7 +7,7 @@ export default async function(req) {
   const currentTime = new Date().toISOString();
 
   console.log('This is my new function!!!');
-  console.log('Goodbye!!! 42 new config who dis');
+  console.log('Goodbye!!! 43 more logging');
   console.log(`Current Time: ${currentTime}`);
 
   return new Response(`goodbye world!!!!!!\nCurrent Time: ${currentTime}`, { headers });
