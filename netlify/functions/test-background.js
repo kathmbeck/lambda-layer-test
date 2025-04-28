90cd40efa4dbdebca7849cd8feccb69d42edcf12
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-04-28 12:35:57 -0400

    Update test-background.js

--- netlify/functions/test-background.js
+++ netlify/functions/test-background.js
@@ -2,5 +2,5 @@ export default async (req, context) => {
     const name = process.env.AWS_LAMBDA_FUNCTION_NAME
     console.log(`Hello from ${name}`);
     console.log(":D")
-    return new Response("Hello, world!");
+    return new Response("Hello, world!!!!");
 };
