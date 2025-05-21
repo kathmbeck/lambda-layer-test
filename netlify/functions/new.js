072e1c17d9ef87a780c70f06b1d813d65b1cfb04
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-05-21 16:39:14 -0400

    Update new.js

--- netlify/functions/new.js
+++ netlify/functions/new.js
@@ -1,12 +1,17 @@
-export default async function (req) {
-  const headers = new Headers({
-    "netlify-cdn-cache-control": "durable, max-age=3600",
-    "netlify-vary": query = "",
+import { trace } from "@opentelemetry/api";
+import { wrapTracer } from "@opentelemetry/api/experimental";
+
+export default function handler(request) {
+  const tracer = wrapTracer(trace.getTracer("example-tracer"));
+  return tracer.withActiveSpan("hello world", async (span) => {
+    await tracer.withActiveSpan("something is slow here", async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    });
+
+    return Response.json("Hello world");
   });
-
-  // Generate a response that exceeds 150,000 bytes
-  const largeResponse = "A".repeat(160000); // 160 KB of "A"s
-
-  return new Response(largeResponse, { headers });
 }
 
+export const config = {
+  path: "/*",
+};
