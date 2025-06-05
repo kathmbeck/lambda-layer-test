332cd1a4c5cee4b5e50ee56d14139a884c4da90f
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-06-05 16:48:49 -0400

    Update goodbye-world.js

--- netlify/functions/goodbye-world.js
+++ netlify/functions/goodbye-world.js
@@ -4,10 +4,10 @@ export default async function(req) {
   });
 
   const url = new URL(req.url);
-  console.log('This is not goodbye! Time to say hello!!!!!!');
+  console.log('This is not goodbye! Time to say hello!!!!!!!!!!!!!');
   const currentTime = new Date().toISOString();
 
-  return new Response(`goodbye world!!!!!!\nCurrent Time: ${currentTime}`, { headers });
+  return new Response(`goodbye world!!!!!!!!!\nCurrent Time: ${currentTime}`, { headers });
 }
 
 // Logs outside of the handler
