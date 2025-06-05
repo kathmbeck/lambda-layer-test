1b8b6a3d22510d17fbac8845d08537771af9cca0
Katherine Beck <49894658+kathmbeck@users.noreply.github.com>
2025-06-05 16:49:12 -0400

    Update new.js

--- netlify/functions/new.js
+++ netlify/functions/new.js
@@ -3,8 +3,8 @@ import { wrapTracer } from "@opentelemetry/api/experimental";
 
 export default function handler(request) {
   const tracer = wrapTracer(trace.getTracer("example-tracer"));
-  return tracer.withActiveSpan("hello world", async (span) => {
-    await tracer.withActiveSpan("something is slow here", async () => {
+  return tracer.withActiveSpan("hello worldddddd", async (span) => {
+    await tracer.withActiveSpan("somethinggggg is slow here", async () => {
       await new Promise((resolve) => setTimeout(resolve, 1000));
     });
 
